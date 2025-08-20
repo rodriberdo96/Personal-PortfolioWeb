be73a86d3fea5d9aa305366504aeb3e5a6c96f67
Rodri Berdomas <rodriberdomas@gmail.com>
2025-08-20 15:53:24 -0300

    fixed themetoggle animation

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import {Moon, Sun} from 'lucide-react';
+import { Moon, Sun } from 'lucide-react';
 import { cn } from '../lib/utils';
 
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
     useEffect(() => {
         const storedTheme = localStorage.getItem('theme');
         if (storedTheme === 'dark') {
-            document.documentElement.classList.add('dark');   
+            document.documentElement.classList.add('dark');
             setIsDarkMode(true);
         } else {
             document.documentElement.classList.remove('dark');
             setIsDarkMode(false);
         }
     }, []);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     const toggleTheme = () => {
         if (isDarkMode) {
             document.documentElement.classList.remove('dark');
-            localStorage.setItem('theme', 'light'); 
+            localStorage.setItem('theme', 'light');
             setIsDarkMode(false);
         } else {
             document.documentElement.classList.add('dark');
@@ -25,10 +36,22 @@ export const ThemeToggle = () => {
             setIsDarkMode(true);
         }
     };
+
     return (
-        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-2 right-5 z-50 p-2 rounded-full transition-colors duration-300", 
-    "focus:outline-hidden")}>
-            {isDarkMode ? <Sun className='h-6 w-6 text-yellow-300'/> : <Moon className='h-6 w-6 text-blue-900'/>}
+        <button
+            onClick={toggleTheme}
+            className={cn(
+                "fixed max-sm:hidden top-2 p-2 right-5 z-50 rounded-full transition-all duration-300 focus:outline-hidden",
+                    isScrolled
+                        ? "top-[8px] bg-background/80 shadow-xs backdrop-blur-md p-2"
+                        : "top-[15px] bg-transparent p-2"
+            )}
+        >
+            {isDarkMode ? (
+                <Sun className="h-6 w-6 text-yellow-300" />
+            ) : (
+                <Moon className="h-6 w-6 text-blue-900" />
+            )}
         </button>
     );
-}
+};
