75ff946400f25b5acb52fcaad3f17f83e519750e
Rodri Berdomas <rodriberdomas@gmail.com>
2025-12-15 17:45:16 -0300

    changed cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
                                 {" "}
                                 Get in Touch
                             </a>
-                            <a href="/_Rodrigo Berdomas CV English (8).pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 tansition-colors duration-300">
+                            <a href="/public/Rodrigo Berdomas CV English (2).pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 tansition-colors duration-300">
                                 {" "}
                                 Download CV
                             </a>
