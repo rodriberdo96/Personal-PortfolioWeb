38c174430b308d05b682b8c92bba20b37597e3da
Rodri Berdomas <rodriberdomas@gmail.com>
2025-12-15 17:51:41 -0300

    .

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
                                 {" "}
                                 Get in Touch
                             </a>
-                            <a href="/public/Rodrigo Berdomas CV English (2).pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 tansition-colors duration-300">
+                            <a href="/Rodrigo Berdomas CV English (2).pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 tansition-colors duration-300">
                                 {" "}
                                 Download CV
                             </a>
